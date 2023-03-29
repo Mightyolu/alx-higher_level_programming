6f85ab2b531046979c8c0a725ef36ba363dc50e4
Mightyolu <idowuolumide30@gmail.com>
2023-03-29 05:10:10 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""Square class definition"""
+
+"""Define a class Square."""
 
 
 class Square:
-    """Represents a square
-    Attributes:
-        __size (int): size of a side of the square
-    """
-    def __init__(self, size):
-        """Initializes a square
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
         Args:
-            size (int): size of a side of the square
-        Returns: None
+            size (int): The size of the new square.
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
